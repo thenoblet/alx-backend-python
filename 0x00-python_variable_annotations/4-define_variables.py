2c0c2b03bd89da842d70b80e84a23f1581fe9f11
noblet <appiahpatricknoblet@gmail.com>
2024-08-01 15:32:20 +0100

    Added variable annotation in inline docstrings

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+"""
+This module demonstrates the use of variable annotations in Python.
+
+It includes examples of annotated variables with different data types:
+- `int`
+- `float`
+- `bool`
+- `str`
+"""
+
+
+a: int = 1
+"""
+An integer variable `a` with a value of 1.
+"""
+
+pi: float = 3.14
+"""
+A floating-point variable `pi` representing the mathematical constant
+Pi with a value of 3.14.
+"""
+
+i_understand_annotations: bool = True
+"""
+A boolean variable `i_understand_annotations` indicating whether the concept
+of annotations is understood, set to True.
+"""
+
+school: str = "Holberton"
+"""
+A string variable `school` representing the name of the school,
+set to "Holberton".
+"""
