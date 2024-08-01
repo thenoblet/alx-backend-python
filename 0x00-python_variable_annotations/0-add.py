6c08292fd6b8608b28167599e2d2979bb0053088
noblet <appiahpatricknoblet@gmail.com>
2024-08-01 14:41:35 +0100

    Added a type annotated function that adds to floats

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+"""
+This module provides basic arithmetic operations.
+
+It currently contains a single function `add` which adds two numbers together.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Add two numbers together.
+
+    This function takes two floating-point numbers as input
+    and returns their sum.
+
+    Parameters:
+    a (float): The first number to add.
+    b (float): The second number to add.
+
+    Returns:
+    float: The sum of the two numbers.
+    """
+    return a + b
