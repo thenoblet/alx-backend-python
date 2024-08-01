879a5e7fda1282d2dedfef0ef1512deac6c6f5d8
noblet <appiahpatricknoblet@gmail.com>
2024-08-01 15:23:33 +0100

    Added a function that return the string representation of a floating point value

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+"""
+This module provides basic type conversion utilities.
+
+It includes a function `to_str` that converts a floating-point
+number to a string.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Convert a floating-point number to its string representation.
+
+    This function takes a floating-point number as input and returns
+    its string representation.
+
+    Parameters:
+    n (float): The floating-point number to convert.
+
+    Returns:
+    str: The string representation of the input number.
+    """
+    return str(n)
