784a1303e4bc47a32170932a4b723dfd299d356b
noblet <appiahpatricknoblet@gmail.com>
2024-08-07 22:19:11 +0100

    Added a coroutine that asynchronously generate 10 random float numbers between 0 and 10.

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -9,10 +9,10 @@ It includes the `async_generator` function, which generates a sequence of
 
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     Asynchronously generate 10 random float numbers between 0 and 10.
 
