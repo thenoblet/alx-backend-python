5f2c7edb0edb3e23c24f72fdd50866a2743874f2
Patrick A. Noblet <appiahpatricknoblet@gmail.com>
2024-08-07 01:36:30 +0000

    Update 3-tasks.py

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -5,12 +5,11 @@ Import wait_random from 0-basic_async_syntax.
 """
 
 import asyncio
-from typing import Task
 
 wait_random = __import__("0-basic_async_syntax").wait_random
 
 
-def task_wait_random(max_delay: int) -> Task:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """
     This function returns an asyncio.Task object
     """
